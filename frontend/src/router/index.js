de017aec6fbd3881cc6ed8e86c6e8fe9e83e7a05
linus22765566 <linus22765566@gmail.com>
2021-10-18 21:19:54 +0800

    初版

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Form from '../views/Form.vue'
+// import { createRouter, createWebHistory } from "vue-router";
 
 Vue.use(VueRouter)
 
@@ -17,6 +19,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/formla',
+    name: 'Form',
+    component: Form
+    
   }
 ]
 
---
+++ frontend/src/views/Form.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="form">
+    <img alt="Vue logo" src="../assets/NYCU.jpg" class="mb-3">
+    <Formla User="Linus"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import Formla from '@/components/Formla.vue'
+
+export default {
+  name: 'Form',
+  components: {
+    Formla
+  }
+}
+</script>
+
+<style scoped lang="scss">
+img {
+    width: 200px;
+    height: 110px;
+}
+
+</style>
